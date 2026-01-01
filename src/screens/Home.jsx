fc894f8193019c2fe8e610a512bc6c62ceaa9603
hiw4n <terriblehiwan@gmail.com>
2026-01-01 13:10:07 +0100

    feat: update retos and styles

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,25 +1,38 @@
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+//styles
+import { globalStyles as GS } from '../global/styles/Styles.style';
 
 const Home = () => {
+  //TODO: Retox, cambiar más adelante por data dinámica.
+  const retos = [
+    "Retrato con luz de ventana",
+    "Líneas de fuga en la calle",
+    "Fotografía en Blanco y Negro",
+    "Primer plano de una textura",
+    "Captura un reflejo en el agua"
+  ];
+  const [reto, setReto] = useState("¿Listo para un reto?");
+
+  const generarReto = () => {
+    const indiceAleatorio = Math.floor(Math.random() * retos.length);
+    setReto(retos[indiceAleatorio]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Pantalla de Inicio (Retos)</Text>
+    <View style={GS.containerCenter}>
+      <Text style={GS.title}>Reto del día:</Text>
+      
+      <View style={GS.card}>
+        <Text style={GS.retoText}>{reto}</Text>
+      </View>
+
+      <TouchableOpacity style={GS.button} onPress={generarReto}>
+        <Text style={GS.buttonText}>GENERAR RETO</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center',
-    backgroundColor: '#393E46' // Luego usaremos tu archivo de colores
-  },
-  title: {
-    color: '#EEEEEE',
-    fontSize: 20,
-    fontWeight: 'bold'
-  }
-});
 
 export default Home;
