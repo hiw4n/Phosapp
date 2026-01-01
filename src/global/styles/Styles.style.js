84d751c2cdc358411be0f1cbdab08427e86f62a2
hiw4n <terriblehiwan@gmail.com>
2026-01-01 13:10:54 +0100

    styles: update general styles TODO_ mejorarlo

--- src/global/styles/Styles.style.js
+++ src/global/styles/Styles.style.js
@@ -14,24 +14,42 @@ const globalStyles = StyleSheet.create({
     fontWeight: 'bold',
     color: C.text,
   },
+  card: {
+    backgroundColor: C.primary,
+    padding: 30,
+    borderRadius: 15,
+    marginVertical: 20,
+    width: '80%',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: C.accent
+  },
+  retoText: { 
+    color: C.background, 
+    fontSize: 18, 
+    textAlign: 'center', 
+    fontWeight: 'bold' 
+  },
+  title: { 
+    color: C.accent, 
+    fontSize: 14, 
+    letterSpacing: 2 
+  },
 
   //Buttons
   button: {
-    nav:{
-      active: C.accent,
-      inactive: C.inactive,
-    },
-    primary:{
-
-
-    }
-  }
-
-
-
-
-
-
+    backgroundColor: C.accent, 
+    padding: 15, 
+    borderRadius: 10,
+  },
+  buttonText: { 
+    color: 'white', 
+    fontWeight: 'bold' 
+  },
+  nav:{
+    active: C.accent,
+    inactive: C.inactive,
+  },
 
 });
 
